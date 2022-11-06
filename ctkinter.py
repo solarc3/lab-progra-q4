9625fdb651a220d864dd7f4c71fca363ad364fe7
Ignacio Solar <ignacio.solar@usach.cl>
2022-11-06 17:01:59 -0300

    Update ctkinter.py

--- ctkinter.py
+++ ctkinter.py
@@ -1,44 +1,43 @@
-#first phase to simply display tk with open streetmaps on the window
-
 import customtkinter
 from tkintermapview import TkinterMapView
-
-customtkinter.set_default_color_theme("blue")
-
-
-class App(customtkinter.CTk):
-
-    nombre = "mapa"
-    ancho = 800
-    alto = 500
-
-    def __init__(mapa):
-        super().__init__()
-
-        mapa.title(App.nombre)
-        mapa.minsize(App.ancho, App.alto)
-        mapa.grid_columnconfigure(1, weight=1)
-        mapa.grid_rowconfigure(0, weight=1)
-        mapa.frame = customtkinter.CTkFrame(master=mapa)
-        mapa.frame.grid(row=0, column=1, rowspan=1, pady=0, padx=0, sticky="nsew")
-        mapa.frame.grid_rowconfigure(1, weight=1)
-        mapa.frame.grid_columnconfigure(0, weight=1)
-
-        mapa.map_widget = TkinterMapView(mapa.frame)
-        mapa.map_widget.grid(row=1, rowspan=1, column=0, columnspan=3, sticky="nswe", padx=(0, 0), pady=(0, 0))
+from tkintermapview import utility_functions
+from tkinter import messagebox
 
 
-        # Set default values
-        mapa.map_widget.set_address("Maipu")
-        mapa.map_widget.set_zoom(14)
-        
-     def start(mapa):
-        mapa.mainloop()
+app = customtkinter.CTk()
 
 
+def add_marker(cords):
+    real = utility_functions.convert_coordinates_to_address(cords[0], cords[1])
+    print(real.street, real.housenumber, real.latlng, real.postal)
+    if real.postal[0] == "9" and real.postal[1] == "2" and real.postal[2] == "5":
+        app.map_widget.set_marker(cords[0], cords[1])
+        print("esta en mmaipu!!!!")
+    else:
+        messagebox.showinfo("Error","El lugar seleccionado no esta en maipu")
+        print("no esta en maipu!!!")
 
 
+app.title("app")
+app.minsize(800, 500)
 
-if __name__ == "__main__":
-    app = App()
-    app.start()
+app.grid_columnconfigure(0, weight=0)  # primera columna
+app.grid_columnconfigure(1, weight=1)  # segunda columna
+app.grid_rowconfigure(0, weight=1)  # las filas no cambian
+app.frame = customtkinter.CTkFrame()
+app.frame.grid(row=0, column=1, sticky="nsew")
+app.frame.grid_rowconfigure(1, weight=1)
+app.frame.grid_columnconfigure(0, weight=1)
+
+app.map_widget = TkinterMapView(app.frame)
+app.map_widget.grid(row=1, column=0, sticky="nswe")
+# direccion default
+app.map_widget.set_address("Maipu")
+app.map_widget.set_zoom(14)
+app.map_widget.add_right_click_menu_command(label="Añadir un marcador",
+                                command=add_marker,
+                                pass_coords=True)
+
+ 
+app.mainloop()
+# end
